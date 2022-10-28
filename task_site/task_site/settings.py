ec5e4b4c7f274c47a189641484063ab045d7dd6c
Simeone Umoh <simeoneumoh@outlook.com>
2022-10-28 18:37:32 +0100

    dotenv

--- task_site/task_site/settings.py
+++ task_site/task_site/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+ENV_PATH = BASE_DIR / '.env'
+
+load_dotenv(ENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
